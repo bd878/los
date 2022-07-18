05a695d438db8fbc938165dc5535110cd18f7b37
user <slavad878@gmail.com>
2022-07-18 16:11:46 +0300

    update comments in clk_tck

--- clk_tck.c
+++ clk_tck.c
@@ -8,7 +8,14 @@ int
 main(void)
 {
   long val;
+  /* sysconf tells real limits on this system */
+  /* some limits are unchangeable, others are */
+  /* pathconf, fpathconf tells limits for the filesystem */
+  /* the pointed file resides in */
+
   if ((val = sysconf(_SC_CLK_TCK)) < 0) {
+    /* whether sysconf fail, return -1 and set errno var */
+    /* or return positive value */
     if (errno != 0) {
       if (errno == EINVAL) {
         printf("not supported\n");
