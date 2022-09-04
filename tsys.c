ff071ff77a1885f54417dc2a89b11b84f1189339
user <slavad878@gmail.com>
2022-09-04 11:49:25 +0300

    add explaining comments to tsys

--- tsys.c
+++ tsys.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[])
   if (argc < 2)
     err_quit("at least one command argument required");
 
+  // we call given command (printuids) via system procedure
+  // to show that system keeps effective uid (of tsys).
+  // It is considered a security breache since
+  // gives child processes root permissions.
+  // Use fork and exec to switch for raised permissions
   if ((status = system(argv[1])) < 0)
     err_sys("failed to call system()");
 
