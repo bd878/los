d5a95bec2f6fa00fd3ebe26f6d3dd68e06003653
teralion <slavad878@gmail.com>
2022-12-22 09:07:32 +0300

    implement popen

---
+++ popen_pclose.c
@@ -0,0 +1,86 @@
+#include "lib.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+/*
+ * pointer to array, allocated
+ * on runtime
+ **/
+static pid_t *childpid = NULL;
+
+/*
+ * receive from open_max()
+ **/
+static int maxfd;
+
+FILE *
+popen(const char *cmdstring, const char *type)
+{
+  int i;
+  int pfd[2];
+  pid_t pid;
+  FILE *fp;
+
+  /* r or w */
+  if ((type[0] != 'r' && type[0] != 'w') || type[1] != 0) {
+    errno = EINVAL;
+    return(NULL);
+  }
+
+  if (childpid == NULL) { /* the most first call */
+    /* allocate pointers array, filled with 0 */
+    maxfd = open_max();
+    if ((childpid = calloc(maxfd, sizeof(pid_t))) == NULL)
+      return(NULL);
+  }
+
+  if (pipe(pfd) < 0)
+    return(NULL); /* errno is set via pipe() */
+  if (pfd[0] >= maxfd || pfd[1] >= maxfd) {
+    close(pfd[0]);
+    close(pfd[1]);
+    errno = EMFILE;
+    return(NULL);
+  }
+
+  if ((pid = fork()) < 0) {
+    return(NULL); /* errno is set via fork() */
+  } else if (pid == 0) { /* child proc */
+    if (*type == 'r') {
+      close(pfd[0]);
+      if (pfd[1] != STDOUT_FILENO) {
+        dup2(pfd[1], STDOUT_FILENO);
+        close(pfd[1]);
+      }
+    } else {
+      close(pfd[1]);
+      if (pfd[0] != STDIN_FILENO) {
+        dup2(pfd[0], STDIN_FILENO);
+        close(pfd[0]);
+      }
+    }
+
+    /* close all descriptors in childpid[] */
+    for (i = 0; i < maxfd; i++)
+      if (childpid[i] > 0)
+        close(i);
+
+    execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
+    _exit(127);
+  }
+
+  /* parent process */
+  if (*type == 'r') {
+    close(pfd[1]);
+    if ((fp = fdopen(pfd[0], type)) == NULL)
+      return(NULL);
+  } else {
+    close(pfd[0]);
+    if ((fp = fdopen(pfd[1], type)) == NULL)
+      return(NULL);
+  }
+
+  childpid[fileno(fp)] = pid; /* remember child pid for this fd */
+  return(fp);
+}
