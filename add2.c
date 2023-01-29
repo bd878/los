e6f66ec9e48d8533c7f99457822efdd227fa1152
teralion <slavad878@gmail.com>
2023-01-29 08:40:01 +0300

    add2 with stdio as a soproc (blocks)

--- add2.c
+++ add2.c
@@ -1,21 +1,25 @@
 #include "lib.h"
 
+/*
+ * This filter will not work
+ * as a soprocess, because
+ * standard i/o library buffering
+ * input&output. While main proc
+ * is waiting result, soproc bufferes output
+ */
 int
 main(void)
 {
   int n, int1, int2;
   char line[MAXLINE];
 
-  while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0) {
-    line[n] = 0; /* terminate a string by \0 */
+  while (fgets(line, MAXLINE, stdin) != NULL) {
     if (sscanf(line, "%d%d", &int1, &int2) == 2) {
-      sprintf(line, "%d\n", int1 + int2);
-      n = strlen(line);
-      if (write(STDOUT_FILENO, line, n) != n)
-        err_sys("failed to call write");
+      if (printf("%d\n", int1 + int2) == EOF)
+        err_sys("failed to call printfy");
     } else {
-      if (write(STDOUT_FILENO, "faulty args\n", 19) != 19)
-        err_sys("failed to call write");
+      if (printf("wrong args\n") == EOF)
+        err_sys("failed to call printf");
     }
   }
   exit(0);
