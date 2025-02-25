ab00aea75bca680e07795b7a1599653f0d481d14
bd878 <slavad878@gmail.com>
2025-02-25 22:17:53 +0300

    ioctl iface device

--- ioctl/socket.c
+++ ioctl/socket.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <sys/socket.h>
 
 #define STRLITERALLEN(x) (sizeof(""x""))
@@ -10,6 +12,7 @@ int main()
 {
 	int sock;
 	int res;
+	struct ifreq ethreq;
 
 	sock = socket(AF_PACKET, SOCK_RAW | SOCK_CLOEXEC, 0);
 	if (sock < 0)
@@ -18,6 +21,16 @@ int main()
 	res = setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, "enp7s0", STRLITERALLEN("enp7s0"));
 	if (res < 0)
 		err(EXIT_FAILURE, "secsockopt");
+
+	strncpy(ethreq.ifr_name, "enp7s0", IF_NAMESIZE);
+	res = ioctl(sock, SIOCGIFFLAGS, &ethreq);
+	if (res < 0)
+		err(EXIT_FAILURE, "ioctl");
+
+	ethreq.ifr_flags |= IFF_PROMISC;
+	res = ioctl(sock, SIOCSIFFLAGS, &ethreq);
+	if (res < 0)
+		err(EXIT_FAILURE, "ioctl");
 
 	return 0;
 }
